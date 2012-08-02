e70d724d06f4dd68863a7fa7bfcc01aa3c17b9e8
sadkowsr <rafal.sadkowski@gmail.com>
2012-08-03 00:19:11 +0200

    Added PeselGenerate class to generate all pesel number

--- Pesel Generator/src/org/sadkowski/pesel/generator/MainActivity.java
+++ Pesel Generator/src/org/sadkowski/pesel/generator/MainActivity.java
@@ -3,13 +3,17 @@ package org.sadkowski.pesel.generator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+Pesel pesel;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pesel=new Pesel();
+        
         setContentView(R.layout.activity_main);
     }
 
@@ -19,7 +23,16 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void clicked(){
+    public void clicked(View v){
+    	try {
+			pesel.generatePesels(1987, 07, 14, false, 5000);
+			EditText et = (EditText)findViewById(R.id.editText1);
+			et.setText(pesel.getPesels()[0]);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	return;
     }
 
---
+++ Pesel Generator/src/org/sadkowski/pesel/generator/Pesel.java
@@ -0,0 +1,118 @@
+package org.sadkowski.pesel.generator;
+
+/**
+ * @author Rafa³ Sadkowski, rafal.sadkowski@gmail.com, http://sadkowski.org
+ * 
+ */
+public class Pesel {
+	public static boolean KOBIETA = true;
+	public static boolean MEZCZYZNA = false;
+	private static int PESEL_LENGHT = 11;
+	/**
+	 * @param args
+	 */
+	String[] pesels;
+
+	public static void main(String[] args) {
+		Pesel pesel = new Pesel();
+		double x=0;
+		int z=99999;
+		double min=10000,max=0,tmp=0;
+		try {
+			for(int i=0;i<z;i++){
+			tmp=pesel.generatePesels(2010, 07, 14, Pesel.MEZCZYZNA, 5000);
+			if(tmp>max)
+				max=tmp;
+			if(tmp<min)
+				min=tmp;
+			
+			x+=tmp;}
+		} catch (Exception e) {
+			//System.err.println(e);
+		}
+		System.out.println("Œrednio:"+x/z);
+		System.out.println("Minimum:"+min);
+		System.out.println("Maksimum"+max);
+		
+	}
+
+	public long generatePesels(int year, int mounth, int day, boolean plec,
+			int ilosc) throws Exception {
+		long start = System.nanoTime();
+		pesels = new String[ilosc];
+		int[] peselCurrent = new int[PESEL_LENGHT];
+		char[] peselPrint = new char[PESEL_LENGHT];
+		int r;
+		ilosc *= 2;
+		int plecNr = 0;
+		if (plec == Pesel.MEZCZYZNA)
+			plecNr++;
+		int validMonthNr;
+		int validNumber;
+		peselCurrent[0] = year / 10 % 10;
+		peselCurrent[1] = year % 10;
+
+		if (year < 1800) {
+			throw new Exception(
+					"The date is before 1800. Please enter older date");
+
+		} else if (year < 1900) {
+			peselCurrent[2] = mounth / 10 + 8;
+
+		} else if (year < 2000) {
+			peselCurrent[2] = mounth / 10;
+		} else if (year < 2100) {
+			peselCurrent[2] = mounth / 10 + 2;
+		} else if (year < 2200) {
+			peselCurrent[2] = mounth / 10 + 4;
+		} else if (year < 2300) {
+			peselCurrent[2] = mounth / 10 + 6;
+		} else {
+
+			throw new Exception(
+					"The date is after 2300. Please enter earlier date");
+		}
+		peselCurrent[3] = mounth % 10;
+		peselCurrent[4] = day / 10;
+		peselCurrent[5] = day % 10;
+
+		//generate a validNumber
+		validMonthNr = peselCurrent[0] + peselCurrent[4] + 3 * (peselCurrent[1] + peselCurrent[5]) + 7 * peselCurrent[2] + 9 * peselCurrent[3];
+		
+		for(int l=0;l<6;l++){
+			peselPrint[l]=(char)(peselCurrent[l]+48);
+			
+		}
+		for (int i = 0 + plecNr; i < ilosc; i = i + 2) {
+			peselCurrent[6] = i / 1000;
+			r = i % 1000;
+			peselCurrent[7] = r / 100;
+			r = r % 100;
+			peselCurrent[8] = r / 10;
+			peselCurrent[9] = r % 10;
+				
+			validNumber =  peselCurrent[8] + 3	* peselCurrent[9] + 7 * peselCurrent[6]	+ 9 * peselCurrent[7] + validMonthNr;
+			validNumber %= 10;
+			if (validNumber == 0)
+				peselPrint[10] = 48;
+			else
+				peselPrint[10] = (char)(/*10-valid+48)*/58 - validNumber); 
+			//end generate a valideNumber
+			
+			for(int l=6;l<10;l++){
+				peselPrint[l]=(char)(peselCurrent[l]+48);
+			}
+			this.pesels[(i - plecNr)/2] = String.valueOf(peselPrint);
+		}
+		long end = System.nanoTime();
+		//for(int i=0;i<pesels.length;i++){
+			//System.out.println(pesels[i]);
+		//}
+		return (end-start);
+	}
+
+	public String[] getPesels() {
+		return pesels;
+	}
+
+}
