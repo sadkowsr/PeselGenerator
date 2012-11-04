1bd1bb872c5fd789e6c6738484516efa9741c7bd
sadkowsr <rafal.sadkowski@gmail.com>
2012-11-04 15:04:42 +0100

    Added reaction for click elements

--- Pesel Generator/src/org/sadkowski/pesel/generator/ListPeselActivity.java
+++ Pesel Generator/src/org/sadkowski/pesel/generator/ListPeselActivity.java
@@ -1,8 +1,11 @@
 package org.sadkowski.pesel.generator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,10 +20,34 @@ public class ListPeselActivity extends Activity {
         String[] elementy_listy = { "W³asna lista", "",""};
         ListView prosta_lista = (ListView) findViewById(R.id.listView1);
          
-        @SuppressWarnings({ "rawtypes", "unchecked" })
-		ArrayAdapter adapter_listy = new ArrayAdapter(this,
+      //  @SuppressWarnings({ "rawtypes", "unchecked" })
+		ArrayAdapter<String> adapter_listy = new ArrayAdapter<String>(this,
             android.R.layout.simple_list_item_1, elementy_listy);
         prosta_lista.setAdapter(adapter_listy);
+    
+        prosta_lista.setOnItemClickListener(new OnItemClickListener() {
+        	 
+            public void onItemClick(AdapterView<?> arg0, View arg1, int pos,
+                long arg3) {
+     
+            	
+            /*switch (pos) {
+            case 0:
+                Intent startActivityCustomList = new Intent(View_Main.this,
+                    View_Custom_List.class);
+                startActivity(startActivityCustomList);
+                break;
+            }*/
+     
+            }
+
+	
+     
+        });
+    
+    
+    
+    
     }
 
     @Override
